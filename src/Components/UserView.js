1a02a07e4b73f224ec9c0fe56dd7f2780bb28da4
Crystal Rose-Wainstock <crwainstock@gmail.com>
2023-01-24 11:14:38 +0100

    syntax error cleared

--- src/Components/UserView.js
+++ src/Components/UserView.js
@@ -68,7 +68,20 @@ function UserView({ allProjects }) {
         */}
         <div id="project-grid" className="row">
           {searchTerm.query === ""
-            ? (<div>
+            ? allProjects.map((project) => (
+                <div id="project" key={project.id} className="col-sm-3 ">
+                  <h4 id={project.id}>{project.title}</h4>
+                  <img
+                    src={project.image}
+                    onClick={() => handleClick(project.id)}
+                    alt="singleProject"
+                    className="img-fluid rounded"
+                  />
+                </div>
+              ))
+            : searchTerm.list.map((allProjects) => {
+                return (
+                  <div>
                     {allProjects.map((project) => (
                       <div id="project" key={project.id} className="col-sm-3 ">
                         <h4 id={project.id}>{project.title}</h4>
@@ -78,25 +91,12 @@ function UserView({ allProjects }) {
                           alt="singleProject"
                           className="img-fluid rounded"
                         />
-                      </div>) 
-                      : ({searchTerm.list.map((allProjects) => {
-                        return (
-                        <div>
-                          {allProjects.map((project) => (
-                            <div id="project" key={project.id} className="col-sm-3 ">
-                              <h4 id={project.id}>{project.title}</h4>
-                              <img
-                              src={project.image}
-                              onClick={() => handleClick(project.id)}
-                              alt="singleProject"
-                              className="img-fluid rounded"
-                              />
-                            </div>
-                          ))}
-                        </div>
-                        );
-                      })})
-                </div>
+                      </div>
+                    ))}
+                  </div>
+                );
+              })}
+        </div>
       </div>
     </div>
   );
